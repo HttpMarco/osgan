eccec744ed06543f7344bdbd2345c7ddfd1b2d0f
HttpMarco <mirco.lindenau@gmx.de>
2024-03-26 20:37:44 +0100

    Add read and write String method

--- osgan-netty/src/main/java/dev/httpmarco/osgan/networking/buffer/PacketBuffer.java
+++ osgan-netty/src/main/java/dev/httpmarco/osgan/networking/buffer/PacketBuffer.java
@@ -3,9 +3,21 @@ package dev.httpmarco.osgan.networking.buffer;
 import io.netty5.buffer.Buffer;
 import lombok.AllArgsConstructor;
 
+import java.nio.charset.StandardCharsets;
+
 @AllArgsConstructor
 public class PacketBuffer {
 
     private final Buffer buffer;
 
+    public PacketBuffer writeString(String value) {
+        var bytes = value.getBytes(StandardCharsets.UTF_8);
+        this.buffer.writeInt(bytes.length);
+        this.buffer.writeBytes(bytes);
+        return this;
+    }
+
+    public String readString() {
+        return this.buffer.readCharSequence(this.buffer.readInt(), StandardCharsets.UTF_8).toString();
+    }
 }
