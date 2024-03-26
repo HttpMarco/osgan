5fe07506159a3067dd97976df09e94e278b7da1e
HttpMarco <mirco.lindenau@gmx.de>
2024-03-26 20:29:54 +0100

    Add packet buffer

---
+++ osgan-netty/src/main/java/dev/httpmarco/osgan/networking/buffer/PacketBuffer.java
@@ -0,0 +1,11 @@
+package dev.httpmarco.osgan.networking.buffer;
+
+import io.netty5.buffer.Buffer;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PacketBuffer {
+
+    private final Buffer buffer;
+
+}
