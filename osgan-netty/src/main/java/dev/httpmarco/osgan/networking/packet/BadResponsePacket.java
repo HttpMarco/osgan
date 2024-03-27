5f13c47585d5602287a9387ff20b00244199e7b7
HttpMarco <mirco.lindenau@gmx.de>
2024-03-28 00:16:29 +0100

    Add responder system

---
+++ osgan-netty/src/main/java/dev/httpmarco/osgan/networking/packet/BadResponsePacket.java
@@ -0,0 +1,6 @@
+package dev.httpmarco.osgan.networking.packet;
+
+import dev.httpmarco.osgan.networking.Packet;
+
+public class BadResponsePacket implements Packet {
+}
---
+++ osgan-netty/src/main/java/dev/httpmarco/osgan/networking/packet/ChannelResponderRegisterPacket.java
@@ -0,0 +1,9 @@
+package dev.httpmarco.osgan.networking.packet;
+
+import dev.httpmarco.osgan.networking.Packet;
+
+public class ChannelResponderRegisterPacket implements Packet {
+
+    private String registerResponderId;
+
+}
