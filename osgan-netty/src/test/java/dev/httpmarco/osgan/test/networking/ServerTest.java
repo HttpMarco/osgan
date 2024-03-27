f930e417427bd22d2e2ec3d297a3762a6614623e
HttpMarco <mirco.lindenau@gmx.de>
2024-03-27 19:25:24 +0100

    Better tests

--- osgan-netty/src/test/java/dev/httpmarco/osgan/test/networking/ServerTest.java
+++ osgan-netty/src/test/java/dev/httpmarco/osgan/test/networking/ServerTest.java
@@ -1,15 +1,28 @@
 package dev.httpmarco.osgan.test.networking;
 
+import dev.httpmarco.osgan.networking.client.NettyClient;
 import dev.httpmarco.osgan.networking.server.NettyServer;
 import org.junit.jupiter.api.Test;
 
 public class ServerTest {
 
     @Test
-    public void handle() {
+    public void handle() throws InterruptedException {
         var server = NettyServer.builder().build();
 
-        while (server.isAlive()) {
-        }
+        var client = NettyClient.builder()
+                .withHostname("127.0.0.1")
+                .withConnectTimeout(500)
+                .build();
+
+
+        Thread.sleep(1000);
+
+        client.sendPacket(new TestPacket());
+
+        //client.close();
+
+
+        Thread.sleep(10000);
     }
 }
---
+++ osgan-netty/src/test/java/dev/httpmarco/osgan/test/networking/TestPacket.java
@@ -0,0 +1,9 @@
+package dev.httpmarco.osgan.test.networking;
+
+import dev.httpmarco.osgan.networking.Packet;
+
+public class TestPacket implements Packet {
+
+    private String username;
+
+}
