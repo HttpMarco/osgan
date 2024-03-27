f1cad46e484143c844a93b87546ba0cc5cbea47c
HttpMarco <mirco.lindenau@gmx.de>
2024-03-27 19:25:00 +0100

    Add reflections

--- osgan-netty/build.gradle.kts
+++ osgan-netty/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(libs.netty5)
     api(project(":osgan-utils"))
+    api(project(":osgan-reflections"))
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
