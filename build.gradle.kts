9dda7b78372a283a561daaceb2f2d31cf934cca6
HttpMarco <mirco.lindenau@gmx.de>
2024-05-14 17:55:55 +0200

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "dev.httpmarco"
-    version = "1.1.11-SNAPSHOT"
+    version = "1.1.12-SNAPSHOT"
 
     repositories {
         mavenCentral()
