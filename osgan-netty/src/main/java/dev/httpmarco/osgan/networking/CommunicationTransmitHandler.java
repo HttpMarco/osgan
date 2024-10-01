7bdb580f07e6786ff0e62ae32eff21b08ec7bf6f
HttpMarco <mirco.lindenau@gmx.de>
2024-10-01 18:43:23 +0200

    Execute packet results in a separate thread

--- osgan-netty/src/main/java/dev/httpmarco/osgan/networking/CommunicationTransmitHandler.java
+++ osgan-netty/src/main/java/dev/httpmarco/osgan/networking/CommunicationTransmitHandler.java
@@ -4,18 +4,21 @@ import dev.httpmarco.osgan.networking.channel.ChannelTransmit;
 import dev.httpmarco.osgan.networking.packet.Packet;
 import io.netty5.channel.Channel;
 import io.netty5.channel.ChannelHandlerContext;
-import io.netty5.channel.ChannelShutdownDirection;
 import io.netty5.channel.SimpleChannelInboundHandler;
 import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 @AllArgsConstructor
 public final class CommunicationTransmitHandler extends SimpleChannelInboundHandler<Packet> {
+
+    private static final Executor PACKET_THREAD_EXECUTOR = Executors.newCachedThreadPool();
 
     private final Function<Channel, List<ChannelTransmit>> findTransmitFunction;
     private final BiConsumer<Packet, ChannelTransmit> channelTransmitPacketConsumer;
@@ -24,7 +27,7 @@ public final class CommunicationTransmitHandler extends SimpleChannelInboundHand
 
     @Override
     protected void messageReceived(@NotNull ChannelHandlerContext channelHandlerContext, Packet packet) {
-        channelTransmitPacketConsumer.accept(packet, findTransmitFunction.apply(channelHandlerContext.channel()).stream().filter(it -> it.channel().equals(channelHandlerContext.channel())).findFirst().orElse(null));
+        PACKET_THREAD_EXECUTOR.execute(() -> channelTransmitPacketConsumer.accept(packet, findTransmitFunction.apply(channelHandlerContext.channel()).stream().filter(it -> it.channel().equals(channelHandlerContext.channel())).findFirst().orElse(null)));
     }
 
     @Override
